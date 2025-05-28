6e790fd2ddb60a0325edf244053d23a1ef7fbb52
Mationez <138032846+Mationez@users.noreply.github.com>
2025-05-28 03:57:03 -0300

    Update projetox_vendas.sql
    Novas "Vendas!"

--- projetox_vendas.sql
+++ projetox_vendas.sql
@@ -33,7 +33,7 @@ CREATE TABLE `vendas` (
   KEY `id_produto` (`id_produto`),
   CONSTRAINT `vendas_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`),
   CONSTRAINT `vendas_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `vendas` (
 
 LOCK TABLES `vendas` WRITE;
 /*!40000 ALTER TABLE `vendas` DISABLE KEYS */;
-INSERT INTO `vendas` VALUES (1,1,1,'2024-01-10',1),(2,1,2,'2024-01-10',2),(3,2,3,'2024-02-05',5),(4,3,4,'2024-02-10',10),(5,2,1,'2024-03-01',1);
+INSERT INTO `vendas` VALUES (1,1,1,'2024-01-10',1),(2,1,2,'2024-01-10',2),(3,2,3,'2024-02-05',5),(4,3,4,'2024-02-10',10),(5,2,1,'2024-03-01',1),(6,1,1,'2024-05-01',2),(7,2,2,'2024-05-02',1),(8,3,3,'2024-05-03',4),(9,4,4,'2024-05-04',1),(10,5,5,'2024-05-05',3),(11,6,6,'2024-05-06',2),(12,7,7,'2024-05-07',1),(13,8,8,'2024-05-08',5),(14,1,9,'2024-05-09',2),(15,2,10,'2024-05-10',1),(16,3,11,'2024-05-11',3),(17,4,12,'2024-05-12',2),(18,5,13,'2024-05-13',1),(19,6,14,'2024-05-14',4);
 /*!40000 ALTER TABLE `vendas` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-26  4:08:01
+-- Dump completed on 2025-05-28  3:50:47
